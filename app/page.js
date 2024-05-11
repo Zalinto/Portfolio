cb6ffa06be425ec2f2731bb3746d3be91a67c271
Joshua De Chavez <75658658+Zalinto@users.noreply.github.com>
2024-05-11 03:40:36 -0700

    Sections

--- app/page.js
+++ app/page.js
@@ -1,24 +1,23 @@
 import HeroSection from "./components/HeroSection";
 import Navbar from "./components/Navbar";
-// import AboutSection from "./components/AboutSection";
-// import ProjectsSection from "./components/ProjectSection";
+import AboutSection from "./components/AboutSection.jsx";
+import ProjectsSection from "./components/ProjectsSections";
 // import EmailSection from "./components/EmailSection";
-// import Footer from "./components/Footer";
+import Footer from "./components/Footer";
 import AchievementsSection from "./components/AchievementsSection.jsx";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-[#121212]">
       <div className="container mt-24 mx-auto px-12 py-4">
-        <h1>tests</h1>
         <Navbar />
         <HeroSection />
         <AchievementsSection />
-        {/* <AboutSection /> */}
-        {/* <ProjectsSection /> */}
+        <AboutSection />
+        <ProjectsSection />
         {/* <EmailSection /> */}
       </div>
-      {/* <Footer /> */}
+      <Footer />
     </main>
   );
 }
